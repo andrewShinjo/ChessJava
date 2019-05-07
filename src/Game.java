e6e070e6e2fef2703e4cbb8d16d8752ba517a6d1
unknown <shinjo1233@gmail.com>
2019-05-06 17:55:27 -0700

    Changed colors.

--- src/Game.java
+++ src/Game.java
@@ -6,42 +6,42 @@ public class Game {
 	private Piece[] blackPieces;
 	
 	public Game() {
-		whitePieces = new Piece[] {
-			new Pawn(Team.WHITE, 0, 100),
-			new Pawn(Team.WHITE, 100, 100),
-			new Pawn(Team.WHITE, 200, 100),
-			new Pawn(Team.WHITE, 300, 100),
-			new Pawn(Team.WHITE, 400, 100),
-			new Pawn(Team.WHITE, 500, 100),
-			new Pawn(Team.WHITE, 600, 100),
-			new Pawn(Team.WHITE, 700, 100),
-			new Rook(Team.WHITE, 0, 0),
-			new Knight(Team.WHITE, 100, 0),
-			new Bishop(Team.WHITE, 200, 0),
-			new Queen(Team.WHITE, 300, 0),
-			new King(Team.WHITE, 400, 0),
-			new Bishop(Team.WHITE, 500, 0),
-			new Knight(Team.WHITE, 600, 0),
-			new Rook(Team.WHITE, 700, 0)
+		blackPieces = new Piece[] {
+			new Pawn(Team.BLACK, 0, 100),
+			new Pawn(Team.BLACK, 100, 100),
+			new Pawn(Team.BLACK, 200, 100),
+			new Pawn(Team.BLACK, 300, 100),
+			new Pawn(Team.BLACK, 400, 100),
+			new Pawn(Team.BLACK, 500, 100),
+			new Pawn(Team.BLACK, 600, 100),
+			new Pawn(Team.BLACK, 700, 100),
+			new Rook(Team.BLACK, 0, 0),
+			new Knight(Team.BLACK, 100, 0),
+			new Bishop(Team.BLACK, 200, 0),
+			new Queen(Team.BLACK, 300, 0),
+			new King(Team.BLACK, 400, 0),
+			new Bishop(Team.BLACK, 500, 0),
+			new Knight(Team.BLACK, 600, 0),
+			new Rook(Team.BLACK, 700, 0)
 		};
 		
-		blackPieces = new Piece[] {
-				new Pawn(Team.BLACK, 0, 600),
-				new Pawn(Team.BLACK, 100, 600),
-				new Pawn(Team.BLACK, 200, 600),
-				new Pawn(Team.BLACK, 300, 600),
-				new Pawn(Team.BLACK, 400, 600),
-				new Pawn(Team.BLACK, 500, 600),
-				new Pawn(Team.BLACK, 600, 600),
-				new Pawn(Team.BLACK, 700, 600),
-				new Rook(Team.BLACK, 0, 700),
-				new Knight(Team.BLACK, 100, 700),
-				new Bishop(Team.BLACK, 200, 700),
-				new Queen(Team.BLACK, 300, 700),
-				new King(Team.BLACK, 400, 700),
-				new Bishop(Team.BLACK, 500, 700),
-				new Knight(Team.BLACK, 600, 700),
-				new Rook(Team.BLACK, 700, 700)	
+		whitePieces = new Piece[] {
+				new Pawn(Team.WHITE, 0, 600),
+				new Pawn(Team.WHITE, 100, 600),
+				new Pawn(Team.WHITE, 200, 600),
+				new Pawn(Team.WHITE, 300, 600),
+				new Pawn(Team.WHITE, 400, 600),
+				new Pawn(Team.WHITE, 500, 600),
+				new Pawn(Team.WHITE, 600, 600),
+				new Pawn(Team.WHITE, 700, 600),
+				new Rook(Team.WHITE, 0, 700),
+				new Knight(Team.WHITE, 100, 700),
+				new Bishop(Team.WHITE, 200, 700),
+				new Queen(Team.WHITE, 300, 700),
+				new King(Team.WHITE, 400, 700),
+				new Bishop(Team.WHITE, 500, 700),
+				new Knight(Team.WHITE, 600, 700),
+				new Rook(Team.WHITE, 700, 700)	
 		};
 		
 		board = new Board();
