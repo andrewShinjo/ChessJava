d6e92c60dac598402cd6246bb60aba87234835e4
Andrew Shinjo <shinjo1233@gmail.com>
2019-06-02 21:44:54 -0700

    Added graph stuff

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 import javafx.scene.paint.Color;
 
 public class Board {
@@ -5,10 +8,24 @@ public class Board {
 	private static final int COL = 8;
 	private static final int ROW = 8;
 	private static final int SIDE = 80;
+	private static final int TILES = COL * ROW;
 	
 	private static final Tile[][] chessBoard = new Tile[COL][ROW];
 	
+	// Implement a graph inside this board class
+	private ArrayList<LinkedList<Tile>> adj;
+	private int verticies;
+	private int edges;
+	
 	public Board() {
+		verticies = TILES;
+		edges=0;
+		adj = new ArrayList<>(TILES);
+		
+		adj.add(0, null);
+		for(int i = 1; i <= TILES; i++) {
+			adj.add(i, new LinkedList<Tile>());
+		}
 		
 		for(int col = 0; col < COL; col++) {
 			for(int row = 0; row < ROW; row++) {	
@@ -28,5 +45,9 @@ public class Board {
 	
 	public int getRow() {
 		return ROW;
+	}
+	
+	public int getSize() {
+		return TILES;
 	}
 }
